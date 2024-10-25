87f5bcd736cea7fa30b57c3f8d792e177926e7c8
Erin Jossy <erinjossy@gmail.com>
2024-10-25 13:53:44 +0530

    Added state machine to be tested

--- Final_code/Final_code.ino
+++ Final_code/Final_code.ino
@@ -4,6 +4,9 @@
 #define RED 9
 #define GREEN 11
 #define BLUE 10
+#define EM 6
+#define LDR A2
+#define PROX 12
 
 Servo M1,M2;
 
@@ -25,12 +28,23 @@ int red_int = 255;
 int green_int = 255;
 int blue_int = 255;
 
+/**
+ * @brief Manually calibrates the intensities of the red, green, and blue LEDs.
+ * 
+ * This function sequentially activates each of the RGB LEDs and measures their
+ * intensities using an respective potentiometer. The measured intensities are printed to the
+ * serial monitor and displayed on an LCD. The function starts by setting the servo
+ * motor position to 0 which is white, then each LED is turned on, its intensity is measured, and
+ * the LED is turned off. The intensities are read from analog pin A2 and stored in
+ * the variables led_red, led_green, and led_blue. The results are printed on the LCD
+ * in the format "R:<red_intensity>G:<green_intensity>B:<blue_intensity>".
+ */
 void ManualCalibrate(){
   M1.write(0);
 
   digitalWrite(RED,HIGH);
   delay(delay_time);
-  led_red = analogRead(A2);
+  led_red = analogRead(LDR);
   Serial.print("RED intensity :");
   Serial.println(led_red);
   delay(delay_time);
@@ -38,7 +52,7 @@ void ManualCalibrate(){
   
   digitalWrite(BLUE,HIGH);
   delay(delay_time);
-  led_blue = analogRead(A2);
+  led_blue = analogRead(LDR);
   Serial.print("BLUE intensity :");
   Serial.println(led_blue); 
   delay(delay_time);
@@ -46,7 +60,7 @@ void ManualCalibrate(){
   
   digitalWrite(GREEN,HIGH);
   delay(delay_time);
-  led_green = analogRead(A2);
+  led_green = analogRead(LDR);
   Serial.print("GREEN intensity :");
   Serial.println(led_green);
   lcd.clear();
@@ -60,17 +74,28 @@ void ManualCalibrate(){
 
 }
 
+/**
+ * @brief Calibrates the red, green, and blue LEDs to their maximum intensities using a simple algorithm.
+ * The algorithm works by setting the PWM values of the LEDs to their maximum values, and then decreasing
+ * the value until the detected intensity is equal to the maximum intensity of the three LEDs.
+ * 
+ * The function first sets the PWM values of the LEDs to their maximum values, and then reads the intensities
+ * of the LEDs and stores them in the variables led_red, led_green, and led_blue. It then finds the maximum
+ * intensity of the three LEDs and stores it in the variable max. It then enters a while loop for each of the
+ * LEDs, decreasing the PWM value until the detected intensity is equal to the maximum intensity. Finally, it
+ * prints the PWM values and the intensities of the LEDs to the serial monitor.
+ */
 void AutoCalibrate(){
   
   red_int = 255;
   green_int = 255;
   blue_int = 255;
   
-  M1.write(0);
+  M1.write(0);  
 
   analogWrite(RED,red_int);
   delay(delay_time);
-  led_red = analogRead(A2);
+  led_red = analogRead(LDR);
   Serial.print("RED intensity :");
   Serial.println(led_red);
   delay(delay_time);
@@ -78,7 +103,7 @@ void AutoCalibrate(){
 
   analogWrite(BLUE,blue_int);
   delay(delay_time);
-  led_blue = analogRead(A2);
+  led_blue = analogRead(LDR);
   Serial.print("BLUE intensity :");
   Serial.println(led_blue); 
   delay(delay_time);
@@ -86,33 +111,19 @@ void AutoCalibrate(){
   
   analogWrite(GREEN,green_int);
   delay(delay_time);
-  led_green = analogRead(A2);
+  led_green = analogRead(LDR);
   Serial.print("GREEN intensity :");
   Serial.println(led_green);
   lcd.clear();
   delay(delay_time);
   digitalWrite(GREEN,LOW);
   
-  int max=0;
-
-  if(led_red>led_blue){
-    if(led_red>led_green){
-      max = led_red;
-    }else{
-      max = led_green;
-    }
-  }else{
-    if(blue_int>green_int){
-      max = led_blue;
-    }else{
-      max = led_green;
-    }
-  }
+  int max = max(led_red, max(led_green, led_blue));
 
   while(led_red<max){
     analogWrite(RED, --red_int);
     delay(delay_time);
-    led_red = analogRead(A2);
+    led_red = analogRead(LDR);
     digitalWrite(RED,LOW);
     delay(delay_time);
   }
@@ -120,7 +131,7 @@ void AutoCalibrate(){
   while(led_green<max){
     analogWrite(GREEN, --green_int);
     delay(delay_time);
-    led_green = analogRead(A2);
+    led_green = analogRead(LDR);
     digitalWrite(GREEN,LOW);
     delay(delay_time);
   }
@@ -128,7 +139,7 @@ void AutoCalibrate(){
   while(led_blue<max){
     analogWrite(BLUE, --blue_int);
     delay(delay_time);
-    led_blue = analogRead(A2);
+    led_blue = analogRead(LDR);
     digitalWrite(BLUE,LOW);
     delay(delay_time);
   }
@@ -153,8 +164,6 @@ void AutoCalibrate(){
 }
 
 void setup() {
-  // put your setup code here, to run once:
-
   M1.attach(5);
   M2.attach(6);
   M1.write(0);
@@ -164,43 +173,86 @@ void setup() {
   pinMode(BLUE,OUTPUT);
   pinMode(GREEN,OUTPUT);
 
-  pinMode(A2,INPUT_PULLUP);
+  pinMode(LDR,INPUT_PULLUP);
 
   Serial.begin(9600);
 
   Serial.println("Setup Done");  
 }
 
-void location(){
+/**
+ * @brief Controls the electromagnet by energizing or de-energizing it.
+ * @param energize: 1 to energize the electromagnet, 0 to de-energize it (default is 0).
+ */
+void EMctrl(int energize = 0){  //energize = 1 for energize, 0 for de-energize
+  digitalWrite(EM,energize);
+}
+
+/**
+ * @brief Identifies the color of the object currently in front of the sensor.
+ * @param color: 0 for general color region identification, 1 to check if red, 2 to check if green, 3 to check if blue
+ * @return true if the color matches the specified color, false otherwise
+ */
+bool location(int color = 0){
   int red_cur, blue_cur, green_cur;
-  
+   
   analogWrite(RED,red_int);
   delay(delay_time);
-  red_cur = analogRead(A2);
+  red_cur = analogRead(LDR);
   delay(delay_time);
   digitalWrite(RED,LOW);
 
   analogWrite(GREEN,green_int);
   delay(delay_time);
-  green_cur = analogRead(A2);
+  green_cur = analogRead(LDR);
   delay(delay_time);
   digitalWrite(GREEN,LOW);
 
   analogWrite(BLUE,blue_int);
   delay(delay_time);
-  blue_cur = analogRead(A2);
+  blue_cur = analogRead(LDR);
   delay(delay_time);
   digitalWrite(BLUE,LOW);
-
-  switch (isColor(red_cur, green_cur, blue_cur)) {
-    case 0: Serial.println("White"); break;
-    case 1: Serial.println("Red"); break;
-    case 2: Serial.println("Green"); break;
-    case 3: Serial.println("Blue"); break;
-  }
-
+    
+  if(color == 1){ // check whether the color is red
+    if(isColor(red_cur, green_cur, blue_cur) == 1){
+      return true;
+    }
+    return false;
+  }else if(color == 2){ //check whether the color is green
+    if(isColor(red_cur, green_cur, blue_cur) == 2){
+      return true;
+    }
+    return false;
+  }else if(color == 3){ //check whether the color is blue
+    if(isColor(red_cur, green_cur, blue_cur) == 3){
+      return true;
+    }
+    return false;
+  }else if (color == 0){  //check what is the color in general for debugging
+    switch (isColor(red_cur, green_cur, blue_cur)) {
+      case 0: Serial.println("White"); break;
+      case 1: Serial.println("Red"); break;
+      case 2: Serial.println("Green"); break;
+      case 3: Serial.println("Blue"); break;
+    }
+    return false;
+  }  
 }
 
+/**
+ * @brief Compares the values of red_cur, green_cur, and blue_cur to determine which color is currently being detected.
+ * 
+ * @param red_cur The current value of the red LED from LDR.
+ * @param green_cur The current value of the green LED from LDR.
+ * @param blue_cur The current value of the blue LED from LDR.
+ * 
+ * @return A value representing the color that is currently being detected.
+ * 0: White
+ * 1: Red
+ * 2: Green
+ * 3: Blue
+ */
 int isColor(int red_cur, int green_cur, int blue_cur) {
   if((red_cur+tol)<(green_cur+blue_cur)/2){
     return 1;
@@ -211,15 +263,14 @@ int isColor(int red_cur, int green_cur, int blue_cur) {
   }else{
     return 0;
   }
-
 }
-int input=-1;
-
-void loop() {
-  if(Serial.available()){
-    char c = Serial.read();
-    if(c!='\n'){
-      input = c -'0';
+int input = -1;
+bool picked = false;
+void loop(){
+  if (Serial.available() > 0) {
+    int inputChar = Serial.read();
+    if (inputChar >= '0' && inputChar <= '9') {
+      input = inputChar - '0';
     }
   }
   if(input<0){
@@ -252,18 +303,55 @@ void loop() {
     input = 0;
   }
   if(input==4){
-    int choice = -1;
+    int pick = -1;
+    int drop = -1;
+
+    Serial.println("Press 1 to pick up at red");
+    Serial.println("Press 2 to pick up at green");
+    Serial.println("Press 3 to pick up at blue");
+
     while(true){
       if(Serial.available()){
         char c = Serial.read();
         if(c!='\n'){
-          input = c -'0';
+          pick = c -'0';
         }
       }
-      Serial.println("Press 1 to pick up at red");
-      Serial.println("Press 2 to pick up at green");
-      Serial.println("Press 3 to pick up at blue");
+      if(pick==1 || pick==2 || pick==3){
+        break;
+      }else{
+        Serial.println("Please select valid option");
+      }
+    }
+
+    Serial.println("Press 1 to drop at red");
+    Serial.println("Press 2 to drop at green");
+    Serial.println("Press 3 to drop at blue");
+
+    while(true){
+      if(Serial.available()){
+        char c = Serial.read();
+        if(c!='\n'){
+          drop = c -'0';
+        }
+      }
+      if(drop==1 || drop==2 || drop==3){
+        break;
+      }else{
+        Serial.println("Please select valid option");
+      }
+    }
+    if(location(pick) && (digitalRead(PROX)==HIGH)){
+      M2.write(180);
+      EMctrl(1);
+      M2.write(0);
+      picked = true;
+    }
+    if(location(drop) && picked){
+      M2.write(180);
+      EMctrl(0);
+      M2.write(0);
+      picked = false;
     }
   }
-
-}
+}  
